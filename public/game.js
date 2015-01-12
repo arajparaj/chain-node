1434aa2441cb3b9e8f69e5c1f0a3eb57e6405de6
Araj P Raju <arajparaj@gmail.com>
2015-01-13 01:53:06 +0530

    Added a 2d-box for the states

--- public/game.js
+++ public/game.js
@@ -1,25 +1,101 @@
-var canvas = document.getElementById("gamebox");
-var context = canvas.getContext("2d");
+
+
 var socket = io();
-
-var opts = {
-  distance : 100,
-  lineWidth : 1,
-  gridColor  : "#000000",
-  caption : false
-};
-new Grid(opts).draw(context);
-
-canvas.addEventListener("click",clickHandler);
+var context,canvas;
+var COLS=10;
+var ROWS=5;
+var box;
 
 function clickHandler (e) {
-	x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
-  	y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+
+//identifies the clicked box
+	y = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+  	x = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
   	x = Math.ceil(x/100);
   	y = Math.ceil(y/100);
+
+//IMPORTANT should emit only Allowed positions
   	socket.emit('pos', {xc: x, yc: y});
 }
 
+//initializing the game state box
+function initGameBox() {
+
+    box=new Array(ROWS);
+    for(var i=0;i<=ROWS;i++){
+        box[i]=new Array(COLS);
+    }
+
+    for(i=1;i<=ROWS;i++){
+        for(var j=1;j<=COLS;j++){
+            box[i][j]={"color":"none","capacity":3,"atoms":0};
+        }
+    }
+
+    for(i=2;i<COLS;i++){
+        box[1][i].capacity=2;
+        box[ROWS][i].capacity=2;
+    }
+
+    for(i=2;i<ROWS;i++){
+        box[i][1].capacity=2;
+        box[i][COLS].capacity=2;
+    }
+
+
+    box[1][1].capacity=1;
+    box[1][COLS].capacity=1;
+    box[ROWS][1].capacity=1;
+    box[ROWS][COLS].capacity=1;
+}
+
+function updatecanvas(){
+//update the canvas after each emit
+//update all box in canvas with 0
+    context.font = "bold 20px sans-serif";
+    context.textBaseline = "top";
+    for(var i=1;i<=ROWS;i++){
+        for(var j=1;j<=COLS;j++){
+            context.fillText(box[i][j].atoms, 50+(j-1)*100, 50+(i-1)*100);
+//            console.log(i,j);
+        }
+    }
+}
+
+function initGrid(){
+
+//grid options
+    var opts = {
+        distance : 100,
+        lineWidth : 1,
+        gridColor  : "#000000",
+        caption : false
+    };
+//Adding the grid to canvas
+    new Grid(opts).draw(context);
+
+}
+function init(){
+
+//var declarations
+    canvas = document.getElementById("gamebox");
+    context = canvas.getContext("2d");
+
+    initGrid();
+//add an event listener to the canvas
+    canvas.addEventListener("click",clickHandler);
+    initGameBox();
+    updatecanvas();
+}
+
+
 socket.on('pos', function(msg){
-    console.log(msg.xc,msg.yc);
+    console.log("clicked pos",msg.xc,msg.yc);
+//IMPORTANT userID
+    box[msg.xc][msg.yc].atoms++;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    initGrid();
+    updatecanvas();
 });
+
+init();
